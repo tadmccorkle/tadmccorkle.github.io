afe8388dc665fa6627b00cd652843b3ba0ebd274
Tad McCorkle <tad.mccorkle@gmail.com>
2022-04-03 20:50:23 -0400

    Fix pre-bundling for dev mode. Update formatting config.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
+import { defineConfig } from "vite";
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		vite: defineConfig({
+			optimizeDeps: {
+				include: ["unified > extend", "unified > is-buffer"],
+			},
+		}),
 	},
 };
 
