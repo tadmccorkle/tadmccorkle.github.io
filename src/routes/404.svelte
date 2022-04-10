6240cd1162a17614b69668c21fea1093e39a3782
Tad McCorkle <tad.mccorkle@gmail.com>
2022-04-09 23:57:48 -0400

    Add 404 page for GH Pages.

---
+++ src/routes/404.svelte
@@ -0,0 +1,22 @@
+<!-- a 404 page for static site on github pages -->
+
+<svelte:head>
+	<title>404 | Tad McCorkle</title>
+</svelte:head>
+
+<div class="center">
+	<h1>404</h1>
+	<h2>
+		{"Page not found"}{" "}
+		<div role="img" aria-label="Pensive emoji">😔</div>
+	</h2>
+	<a href="/">homepage</a>
+</div>
+
+<style>
+	.center {
+		margin: auto;
+		text-align: center;
+		padding: 2.4rem 0;
+	}
+</style>
